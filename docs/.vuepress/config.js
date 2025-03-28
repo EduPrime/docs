839d16923085fdf5457edd492f28c33c824e74d4
hermesalvesbr <hermes.alves@araripina.com.br>
2025-03-28 10:39:46 -0300

    adiciona URL base para GitHub Pages na configuração do VuePress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,10 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
-
 export default defineUserConfig({
   title: 'EduPrime.chat',
   description: 'Mais tempo para o professor',
+  base: '/docs/',  // Adiciona base URL para GitHub Pages
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
