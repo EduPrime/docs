e6f76a30848a3d0092a5f7fca06fae9488ec3e8e
Hermes Alves <379850+hermesalvesbr@users.noreply.github.com>
2025-03-28 12:35:37 +0000

    build

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,8 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
+
 export default defineUserConfig({
-    lang: 'pt-BR',
     title: 'EduPrime.chat',
     description: 'Mais tempo para o professor',
   bundler: viteBundler(),
