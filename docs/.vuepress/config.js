fe212cf7e790829673777abb695a0c783f05af16
hermesalvesbr <hermes.alves@araripina.com.br>
2025-03-28 10:48:24 -0300

    substitui a configuração do VuePress e adiciona documentação completa do EduPrime

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,19 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/',
   title: 'EduPrime.chat',
   description: 'Mais tempo para o professor',
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    logo: '/images/logo.png',
+    navbar: [
+      {
+        text: 'Início',
+        link: '/',
+      },
+    ],
+  }),
+  // Configure o diretório fonte
+  sourceDir: 'docs',
 })
