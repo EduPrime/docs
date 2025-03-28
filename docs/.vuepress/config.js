532c960d753098419d0a6b6d2a2b53e54cd8fb16
hermesalvesbr <hermes.alves@araripina.com.br>
2025-03-28 11:19:11 -0300

    atualiza dependência do Vue para a versão 3.5.13 e remove configurações obsoletas do EduPrime

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,16 +3,6 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  title: 'EduPrime.chat',
-  description: 'Mais tempo para o professor',
-  bundler: viteBundler(),
-  theme: defaultTheme({
-    logo: '/images/logo.png',
-    navbar: [
-      {
-        text: 'Início',
-        link: '/',
-      },
-    ],
-  }),
+    bundler: viteBundler(),
+    theme: defaultTheme(),
 })
