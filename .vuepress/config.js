f6f9b3ec0f8978213787f4dc0216a824e3d7d9bd
hermesalvesbr <hermes.alves@araripina.com.br>
2025-03-28 10:42:20 -0300

    remove arquivos de configuração e documentação do EduPrime

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -5,7 +5,6 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   title: 'EduPrime.chat',
   description: 'Mais tempo para o professor',
-  base: '/docs/',  // Adiciona base URL para GitHub Pages
   bundler: viteBundler(),
   theme: defaultTheme(),
 })
